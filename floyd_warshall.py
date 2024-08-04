1ace2ef5948413acbf844919487d7fddfee53a37
James Hooker <jameshooker1999@live.com>
2024-08-04 22:39:35 +0100

    Fixed recursive implementation of Floyd-Warshall algorithm
    Also did a sizeable refactor

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -1,33 +1,60 @@
 from functools import cache
 import random
 
+
+class hashabledict(dict):
+
+    def __hash__(self) -> int:
+        return hash(frozenset(self.items()))
+
+
 Vertex = int
 Edge = tuple[Vertex, Vertex]
 Weight = int
-AdjacencyMatrix = dict[Edge, Weight]
+AdjacencyMatrix = hashabledict[Edge, Weight]
 
-def make_bidirectional(edges: AdjacencyMatrix) -> AdjacencyMatrix:
-    return edges | {(v, u): w for (u, v), w in edges.items()}
 
 class Graph:
 
-    def __init__(self, n: int, edges: AdjacencyMatrix):
-        self.n = n
-        self.edges = edges
+    def __init__(self, n: int = 0, edges: AdjacencyMatrix = AdjacencyMatrix()):
+        self.n = int(n)
+        self.edges = AdjacencyMatrix(edges)
+
+    def __len__(self) -> int:
+        return self.n
+
+    def __iter__(self):
+        return iter(self.vertices())
+
+    def __bool__(self) -> bool:
+        return bool(self.vertices())
+
+    def vertices(self):
+        return range(self.n)
+
+    def __str__(self) -> str:
+        render_value = str
+        w = max(len(render_value(x)) for x in self.edges.values())
+        lines = (' '.join(render_value(self.edges[i, j]).rjust(w) for j in self)
+                 for i in self)
+        return '\n'.join(lines)
 
     @classmethod
     def mkrand(cls, n: int, *, connected: bool = True):
         if not connected: raise NotImplementedError()
         if n < 0: raise ValueError(n)
-        g = cls(n, {})
-        if n == 0: return g
-        vertices = range(g.n)
+        g = cls(n)
+        if not g: return g
         while not g.is_connected():
-            u, v = random.sample(vertices, k=2)
+            u, v = random.sample(g.vertices(), k=2)
             assert u != v
             g.edges[u, v] = 1
-            g.edges = make_bidirectional(g.edges)
+            g.edges = Graph.make_bidirectional(g.edges)
         return g
+
+    @staticmethod
+    def make_bidirectional(edges: AdjacencyMatrix) -> AdjacencyMatrix:
+        return AdjacencyMatrix(edges | {(v, u): w for (u, v), w in edges.items()})
 
     def is_connected(self):
         if self.n < 0: raise ValueError(self.n)
@@ -41,77 +68,49 @@ class Graph:
 
         while addenda := search():
             visited |= addenda
-        return visited == set(range(self.n))
+        return visited == set(self.vertices())
 
 
-def floyd_warshall(g: Graph) -> AdjacencyMatrix:
+class FloydWarshall:
 
-    vertices = range(g.n)
-    dist: AdjacencyMatrix = {(i, j): float('inf')
-                             for i in vertices for j in vertices}
-    for edge, weight in g.edges.items():
-        u, v = edge
-        dist[u, v] = weight
-    for v in vertices:
-        dist[v, v] = 0
-    for k in vertices:
-        for i in vertices:
-            for j in vertices:
-                w = dist[i, k] + dist[k, j]
-                if w < dist[i, j]:
-                    dist[i, j] = w
-                continue
-                try:
-                    w = dist[i, k] + dist[k, j]
-                except TypeError:
-                    continue
-                try:
-                    if w < dist[i, j]:
-                        dist[i, j] = w
-                except TypeError:
-                    dist[i, j] = w
+    def __init__(self):
+        raise NotImplementedError
 
-    return dist
+    @classmethod
+    def dp(cls, g: Graph) -> AdjacencyMatrix:
+        # By dynamic programming
+        vertices = range(g.n)
+        dist = AdjacencyMatrix({(i, j): float('inf')
+                                for i in g for j in g})
+        for edge, weight in g.edges.items():
+            u, v = edge
+            dist[u, v] = weight
+        for v in vertices:
+            dist[v, v] = 0
+        for k in range(g.n):
+            for i in g:
+                for j in g:
+                    dist[i, j] = min(dist[i, j], dist[i, k] + dist[k, j])
+        return dist
 
+    @classmethod
+    def recursive(cls, g: Graph) -> AdjacencyMatrix:
+        # By recursion
+        return {(i, j): cls.shortestPath(g.edges, i, j, g.n)
+                for i in g for j in g}
 
-def floyd_warshall2(g: Graph) -> AdjacencyMatrix:
-    vertices = range(g.n)
-    return {(i, j): shortestPath(g.edges, i, j, g.n)
-            for i in vertices for j in vertices}
-
-
-# @cache
-def shortestPath(edges: AdjacencyMatrix, i: Vertex, j: Vertex, k: int) -> Weight:
-    if k == 0:
-        return 0 if i == j else edges.get((i, j), float('inf'))
-
-    a = shortestPath(edges, i, j, k - 1)
-    b = shortestPath(edges, i, k, k - 1)
-    c = shortestPath(edges, k, j, k - 1)
-    # if b + c < a:
-        # print(f'At {k = }, found path from {i} to {j} of length {b + c}, which is shorter than {a}')
-    return min(a, b + c)
-
-    return min(  shortestPath(edges, i, j, k - 1),
-                 shortestPath(edges, i, k, k - 1)
-               + shortestPath(edges, k, j, k - 1))
+    @classmethod
+    @cache
+    def shortestPath(cls, edges: AdjacencyMatrix, i: Vertex, j: Vertex, k: int) -> Weight:
+        if k < 0: return int(i != j) and edges.get((i, j), float('inf'))
+        return min(  cls.shortestPath(edges, i, j, k - 1),
+                     cls.shortestPath(edges, i, k, k - 1)
+                   + cls.shortestPath(edges, k, j, k - 1))
 
 
-def render(n: int, dist: AdjacencyMatrix) -> str:
-    render_weight = str
-    w = max(len(render_weight(x)) for x in dist.values())
-    lines = [
-        ' '.join(render_weight(dist[i, j]).rjust(w)
-                 for j in range(n)) for i in range(n)
-    ]
-    return '\n'.join(lines)
+def demo():
 
-
-if __name__ == '__main__':
-
-    n = 8
-
-    edges = {
+    g = Graph(8, Graph.make_bidirectional({
         (0, 2): 1,
         (1, 2): 1,
         (1, 3): 1,
@@ -120,39 +119,28 @@ if __name__ == '__main__':
         (4, 5): 1,
         (5, 6): 1,
         (1, 7): 1,
-    }
-
-    edges = make_bidirectional(edges)
-
-    g = Graph(n, edges)
+    }))
     assert g.is_connected()
 
-    g = Graph(4, make_bidirectional({
+    g = Graph(4, Graph.make_bidirectional({
         (0, 1): 1,
         (1, 2): 1,
         (2, 3): 1,
     }))
+    assert g.is_connected()
 
-    # g = Graph.mkrand(4)
-    g = Graph(4, make_bidirectional({(0, 1): 1, (0, 2): 1, (2, 3): 1}))
-    print(g.is_connected())
-    # print(g.edges)
-    # print(render(g.n, g.edges))
+    g = Graph.mkrand(4)
+    # g = Graph(4, Graph.make_bidirectional({(0, 1): 1, (0, 2): 1, (2, 3): 1}))
 
-    x = floyd_warshall(g)
-    y = floyd_warshall2(g)
+    x = FloydWarshall.dp(g)
+    y = FloydWarshall.recursive(g)
+    assert x == y, 'Algorithms differ!'
 
     print(g.edges, end='\n\n')
-    print(render(g.n, x), end='\n\n')
-    print(render(g.n, y), end='\n\n')
-    if x != y:
-        print('Algorithms differ!')
+    print(Graph(g.n, x), end='\n\n')
+    print(Graph(g.n, y), end='\n\n')
 
-    # vertices = range(g.n)
-    # for k in range(g.n+1):
-    #     print(render(g.n, {(i, j): shortestPath(g.edges, i, j, k)
-    #         for i in vertices for j in vertices}))
-    #     print()
-    exit()
 
-    print(render(g.n, floyd_warshall2(g)))
+if __name__ == '__main__':
+
+    demo()
